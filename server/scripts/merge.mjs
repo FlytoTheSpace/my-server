e6bc802f28db205dfa443f9c071ef73c3479cce7
FlytoTheSpace <flytothespace.64@gmail.com>
2024-01-28 17:01:58 +0530

    Casual Commit

--- server/scripts/merge.mjs
+++ server/scripts/merge.mjs
@@ -1,32 +1,19 @@
-import { random } from 'mathjs';
 import PDFMerger from 'pdf-merger-js';
+import path from 'path';
 
-var merger = new PDFMerger();
-
-const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwyxz";
-const generateName = () => {
-    let pdfname = "";
-    for (let i = 0; i <= 11; i++) {
-        let randomChar = Math.floor(
-            Math.random() * characters.length
-        );
-        pdfname += characters[randomChar];
-    }
-    return pdfname;
-};
-
+export const publicDir = "./client/static/public"
 export const mergePDFs = async (pdf1, pdf2) => {
-    const PDFname = generateName(); // Generate a new name at the time of merging
+    const merger = new PDFMerger();
+    const mergedPDFFileName = Date.now(); // Generate a new name at the time of merging
+    try{
 
-    await merger.add(pdf1);
-    await merger.add(pdf2);
+        await merger.add(pdf1);
+        await merger.add(pdf2);
 
-    await merger.save(`./client/static/public/${PDFname}.pdf`); // Save under the generated name and reset the internal document
-
-    // Export the merged PDF as a nodejs Buffer
-    // const mergedPdfBuffer = await merger.saveAsBuffer();
-    // fs.writeSync('merged.pdf', mergedPdfBuffer);
-
-    return PDFname; // Return the generated name if needed
+        await merger.save(path.join(publicDir, mergedPDFFileName)); // Save under the generated name and reset the internal document
+    } catch (error){
+        console.log("[Merge.mjs]", error)
+    }
+    return mergedPDFFileName; // Return the generated name if needed
 };
 
--- src/test.ts
+++ src/test.ts
@@ -1,27 +1,24 @@
-const path = require('path')
-const fs = require('fs')
-const ROOT = require('./assets/root')
-const GetABCChars = () => {
-    const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+import path from 'path'
+import fs from 'fs'
 
-    Count = 18
+// import ROOT from './assets/root'
+const GetABCChars = (Count: number) => {
+    const chars = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
     for (let i = 0; i <= Count; i++) {
         console.log(chars[i])
     }
 }
+function sum(a: number, b: number): number {
+    return a + b
+}
 
-// (async () => {
-//     const Filetxt = fs.readFileSync(path.join(ROOT, 'config.json'), 'utf8')
-//     const File = Buffer.from(Filetxt)
-//     console.log(File)
-//     fs.writeFileSync(path.join(ROOT, 'config2.json'),File)
-// })()
+enum Direction {
+    Up = "Up",
+    Down = "Down",
+    Left = "Left",
+    Right = "Right"
+}
 
-const buf = Buffer.from("Hello World");
-console.log(buf) // <Buffer 48 65 6c 6c 6f 20 57 6f 72 6c 64>
-console.log(buf.toString('utf8')); // "Hello Wolrd"
-
-buf.write("Hi", 0); // Overwrites the buffer with "Hi" at the beginning
-console.log(buf) // <Buffer 48 69 6c 6c 6f 20 57 6f 72 6c 64>
-console.log(buf.toString('utf8')) // Hillo World
+const direction: Direction = Direction.Up;
+console.log(direction)
